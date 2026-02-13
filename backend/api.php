5e9e1874fd59c3a0b019b5134dec9b394a58f226
in2dw3 <in2dw3@elorrieta.int>
2026-02-13 17:09:26 +0100

    Entrega Final

--- backend/api.php
+++ backend/api.php
@@ -7,6 +7,8 @@ header('Access-Control-Allow-Origin: *'); // para demos; en prod restringe
 header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   http_response_code(204);
   exit;
